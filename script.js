ba5583b7d911a7b89e99aff7054739acd0d4cdba
Matthijs <matthijs.vannistelrooij@gmail.com>
2023-10-20 22:14:44 +0200

    update cupcounter

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ class AudioController {
   constructor() {
     this.bgMusic = new Audio("audio/elevator-music.mp3")
     this.victorySound = new Audio("audio/victory.mp3")
-    this.gameOverSound = new Audio("audio/gameover.wav")
+    this.gameOverSound = new Audio("audio/gameover.mp3")
     this.bgMusic.volume = 0.1
-    this.bgMusic.loop = true
+    this.bgMusic.loop = false
   }
   startMusic() {
     this.bgMusic.play()
+  }
+  stopMusic() {
+    this.bgMusic.pause()
+    this.bgMusic.currentTime = 0
   }
   victory() {
     this.stopMusic()
@@ -18,31 +22,27 @@ class AudioController {
     this.gameOverSound.play()
   }
 }
+
 class MixOrMatch {
   constructor(totalTime) {
     this.totalTime = totalTime
     this.timeRemaining = totalTime
     this.timer = document.getElementById("time-remaining")
-    this.ticker = document.getElementById("flips")
+    this.ticker = document.getElementById("cups")
     this.audioController = new AudioController()
-    this.flips = 0 // Add the flips variable
+    this.cups = 0 // Add the cups variable
   }
 
   startGame() {
     this.totalClicks = 0
-    this.flips = 0 // Initialize flips count to 0
+    this.cups = 0 // Initialize cups count to 0
     this.timeRemaining = this.totalTime
     setTimeout(() => {
       this.audioController.startMusic()
       this.countDown = this.startCountDown()
     }, 500)
     this.timer.innerText = this.timeRemaining
-    this.ticker.innerText = this.flips // Update the ticker to display flips count
-  }
-
-  increaseFlips() {
-    this.flips++
-    this.ticker.innerText = this.flips // Update the displayed flips count
+    this.ticker.innerText = this.cups // Update the ticker to display cups count
   }
 
   startCountDown() {
@@ -64,6 +64,11 @@ class MixOrMatch {
     this.audioController.victory()
     document.getElementById("victory-text").classList.add("visible")
   }
+  
+  increaseCups() {
+    this.cups++
+    this.ticker.innerText = this.cups // Update the displayed cups count
+  }
 }
 
 function ready() {
@@ -84,14 +89,11 @@ if (document.readyState === "loading") {
   ready()
 }
 
-function checkAllItemsPlaced() {
-  return itemsPlaced === 10 // Assuming you have 10 items to be placed
-}
-
 document.addEventListener("DOMContentLoaded", function () {
   const smallImages = document.querySelectorAll(".small-images img")
   const dropContainer = document.querySelector(".drop-container")
   let itemsPlaced = 0
+  let game // Declare the game instance
 
   // Add drag and drop event listeners
   smallImages.forEach((smallImage) => {
@@ -118,14 +120,15 @@ document.addEventListener("DOMContentLoaded", function () {
       const dropSound = document.getElementById("dropSound")
       dropSound.play()
 
-      // Check if all items are placed
+      // Increase the cups count by calling the increaseCups function
+      game.increaseCups()
+
       if (itemsPlaced === 10) {
-        // Play the "all items placed" sound
-        const allItemsPlacedSound = document.getElementById(
-          "allItemsPlacedSound"
-        )
-        allItemsPlacedSound.play()
+        game.victory() // Call the victory function from MixOrMatch
       }
     }
   })
+
+  // Initialize the game
+  game = new MixOrMatch()
 })
